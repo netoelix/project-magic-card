3964814d95a8a82cbfbe63474c7bac1371bbd8d1
Elix Neto <neto.elix@outlook.com>
2023-09-09 20:23:59 -0300

    Finalizando testes do arquivo favorite.tests.js

--- src/__tests__/favorite.test.js
+++ src/__tests__/favorite.test.js
@@ -1,8 +1,8 @@
-// const { saveFavoriteMagicCard } = require('../magic');
-// const favoriteCards = require('../data/favoriteCards');
+const { saveFavoriteMagicCard } = require('../magic');
+const favoriteCards = require('../data/favoriteCards');
 
 const restoreFavoriteCards = () => {
-  // implemente seu código aqui
+  favoriteCards.splice(4);
 };
 
 jest.setTimeout(10000);
@@ -11,20 +11,28 @@ describe('3 - Testes da função saveFavoriteMagicCard', () => {
   afterEach(restoreFavoriteCards);
 
   it('3.1 Testa se um novo card é adicionado a cada execução', async () => {
-    // await saveFavoriteMagicCard('130553');
-    // implemente seus testes aqui
+    await saveFavoriteMagicCard('130553');
+    const arrayLength = favoriteCards.length - 1;
 
+    expect(favoriteCards).toHaveLength(5);
+    expect(favoriteCards[arrayLength].name).toBe('Beacon of Immortality');
+
+    await saveFavoriteMagicCard('130554');
+    expect(favoriteCards).toHaveLength(6);
   });
 
   it('3.2 Deve retornar favoriteCards contendo apenas os cards favoritos iniciais', () => {
-    // expect(favoriteCards).toHaveLength(4);
+    expect(favoriteCards).toHaveLength(4);
   });
 });
 
 describe('4 - Testes da função saveFavoriteMagicCard', () => {
   it(`4.1 Implemente um teste que verifique que o array que você obteve com o map
   contém todos os quatro nomes iniciais nomes`, async () => {
-    // implemente seus testes aqui
-
+    const newArray = ['Ancestor\'s Chosen', 'Angel of Mercy',
+     'Aven Cloudchaser', 'Ballista Squad'];
+    const names = favoriteCards.map(({ name }) => name);
+    
+    expect(newArray).toEqual(names);
   });
 });
